89c9e486e3da5f1f2fb619eba207e4fe6479283e
lauracejas <lauracejasmartin94@gmail.com>
2021-06-24 19:19:21 -0400

    add icon links on contact page

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram } from "react-icons/fa"
 import "./footer.css"
 
 const Footer = () => {
     return(
 <div className="footer">
-<p  id="not-dark" className="Copy text-center">2021 © Copyright <p>Laura Cejas</p>. All Rights Reserved</p>
+<p  id="not-dark" className="Copy text-center">2021 © Samy's Restoration LLC | All Rights Reserved</p>
+
+<Link to="/" >
+                        <FaFacebook className="faceBook" style={{ size:"20px" }}/>
+                        </Link>
+                        <Link to="/">
+                        <FaInstagram className="InstaGram" style={{ color: "darkorange", size:"20px" }}/>
+                        </Link>
 
 </div>
     )
--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { useState } from 'react';
-import  emailjs  from 'emailjs-com';
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaEnvelope, FaPhone } from "react-icons/fa"
+import emailjs from 'emailjs-com';
 import "./contact.css"
 
 const Contact = () => {
@@ -15,18 +16,18 @@ const Contact = () => {
     //     Subject: '',
     //     message: '',
     //   });
-    
-      const sendEmail = (e) => {
+
+    const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm('gmail', 'template_ff2b5zq', e.target, 'user_aJUQ488C6B5o1b0uwSIXz')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-      };
-    
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+
     //   const handleChange = (e) => {
     //     setToSend({ ...toSend, [e.target.name]: e.target.value });
     //   };
@@ -40,51 +41,65 @@ const Contact = () => {
 
                     <div className="typeZone col-md-6 ">
                         <form onSubmit={sendEmail}>
-                            <input 
-                            type="text" 
-                            id="fname" 
-                            name="firstname" 
-                            placeholder="Your name" 
-                            required>                                
+                            <input
+                                type="text"
+                                id="fname"
+                                name="firstname"
+                                placeholder="Your name"
+                                required>
                             </input>
-                            <input 
-                            type="email" 
-                            id="mailid" 
-                            name="email" 
-                            placeholder="Email Address" 
-                            required></input>
-                            <input 
-                            type="text" 
-                            id="sub" 
-                            name="subject" 
-                            placeholder="Subject"
-                            required></input>
-                            <textarea 
-                            id="msg" 
-                            name="message" 
-                            placeholder="How can I help you?" 
-                            
-                            required></textarea>
+                            <input
+                                type="email"
+                                id="mailid"
+                                name="email"
+                                placeholder="Email Address"
+                                required></input>
+                            <input
+                                type="text"
+                                id="sub"
+                                name="subject"
+                                placeholder="Subject"
+                                required></input>
+                            <textarea
+                                id="msg"
+                                name="message"
+                                placeholder="How can I help you?"
+
+                                required></textarea>
                             <input id="sendBut" type="submit" className="btn btn-info-secondary" value="Send Mesage"></input>
                         </form>
-                            {/* <button onSubmit={sendEmail} style={{ cursor: 'pointer' }} className="btn btn-secondary" type="submit"><label style={{ cursor: 'pointer' }} id="not-dark">Send Message</label></button> */}
+                        {/* <button onSubmit={sendEmail} style={{ cursor: 'pointer' }} className="btn btn-secondary" type="submit"><label style={{ cursor: 'pointer' }} id="not-dark">Send Message</label></button> */}
                     </div>
                     <div className="linksMe col-md-6 ">
 
                         <div className="more-info">
                             <p className="lead">
-                            Services from Samy's Restoration are trusted, reliable and professional. Please fill out the form completely, and we will get back to you as soon as possible.
+                                Services from Samy's Restoration are trusted, reliable and professional. Please fill out the form completely, and we will get back to you as soon as possible.
                             </p>
                         </div>
                         <p><i className="fas fa-map-marker-alt"></i> Charlotte, NC</p>
-                        <p><i className="fas fa-phone"></i><a href="tel:9802750597">(704) 681 2083</a></p>
-                        <p><i className="far fa-envelope"></i><a href="mailto:samyselectricllc@gmail.com"> samyselectricllc@gmail.com</a></p>
-                        <div className="buttonz">
-                            <a href="/" target="blank" id="btn" className="button button-outline button-white"><span className="ico-circle">
-                                <i className="ion-social-facebook"></i>
-                            </span></a>
-                            
-                        </div>
+                        <p><Link to="tel:9802750597"><FaPhone />(704) 681 2083</Link></p>
+                        <p><Link to="mailto:samyselectricllc@gmail.com" ><FaEnvelope /> samyselectricllc@gmail.com</Link></p>
+                        {/* <div className="buttonz">
+                            <Link to="/" target="blank" id="btn" className="button button-outline button-white">
+                                
+                              <span className="ico-circle">
+                                    <i className="ion-social-facebook"></i>
+                                </span> 
+                            </Link>
+                            <Link to="/" target="blank" id="btn" className="button button-outline button-white"><span className="ico-circle">
+                                <i className="ion-social-instagram"></i>
+                            </span></Link>
+
+                        </div> */}
+                        <Link to="/" >
+                        <FaFacebook className="faceBook" style={{ color: "darkblue", size:"20px" }}/>
+                        </Link>
+                        <Link to="/">
+                        <FaInstagram className="InstaGram" style={{ color: "darkorange", size:"20px" }}/>
+                        </Link>
+
+
                     </div>
                 </div>
 
