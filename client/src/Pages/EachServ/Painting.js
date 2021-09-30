292c5cb1f87ae90f3e9a36e42179d9c3253eb3a0
lauracejas <lauracejasmartin94@gmail.com>
2021-09-30 17:33:22 -0400

    update pictures

--- client/src/Pages/EachServ/Painting.js
+++ client/src/Pages/EachServ/Painting.js
@@ -2,27 +2,27 @@ import React from "react";
 import "./style.css";
 import cardData from "../../Seeds/services";
 
-// import doors from "../../assets/painting/painting1.jpg";
-// import floor from "../../assets/painting/painting2.jpg";
-// import bathroom from "../../assets/painting/painting3.jpg";
-// import painting from "../../assets/painting/painting4.jpg";
-// import electrical from "../../assets/painting/painting5.jpg";
+import doors from "../../assets/painting/painting1.jpg";
+import floor from "../../assets/painting/painting2.jpg";
+import bathroom from "../../assets/painting/painting3.jpg";
+import painting from "../../assets/painting/painting4.jpg";
+import electrical from "../../assets/painting/painting5.jpg";
 // import roofing from "../../assets/painting/painting6.jpg";
 // import plumbing from "../../assets/painting/painting7.jpg";
 
 
 function image (image) {
     switch (image) {
-        // case 'doors':
-        //     return doors;
-        // case 'floor':
-        //     return floor;
-        // case 'bathroom':
-        //     return bathroom;
-        // case 'painting':
-        //     return painting;
-        // case 'electrical':
-        //     return electrical;
+        case 'doors':
+            return doors;
+        case 'floor':
+            return floor;
+        case 'bathroom':
+            return bathroom;
+        case 'painting':
+            return painting;
+        case 'electrical':
+            return electrical;
         // case 'roofing':
         //     return roofing;
         // case 'plumbing':
@@ -41,7 +41,7 @@ const MyGallery = () => {
             <div className="row text-center cardSection">
             {cardData.map(item => (
                     <div className="card"  key={item.id}>
-                        <img src={image(item.image)} style={{width:"300px", height:"300px"}} className="card-img" alt="images" />
+                        <img src={image(item.image)} style={{width:"320px", height:"300px"}} className="card-img" alt="images" />
                         
                     </div>
                 ))
